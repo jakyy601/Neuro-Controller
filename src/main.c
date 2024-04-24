caa61c6fe3397a864268ea5ae24333fc2e7eb14e
Jakob Schatzl <43982401+jakyy601@users.noreply.github.com>
2024-04-25 00:09:29 +0200

    made static arrays

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@
 
 pthread_t feedInputThread;
 pthread_mutex_t mutex;
-input_st input = (input_st){0};
+input_st input_s = (input_st){0};
 
 void* feedInput(void* arg) {
     FILE* fptr = fopen("pt1.txt", "r");
@@ -22,14 +22,14 @@ void* feedInput(void* arg) {
     int ret = 0;
     char* pEnd = NULL;
     while (1) {
-        if (input.available == false) {
+        if (input_s.available == false) {
             ret = fscanf(fptr, "%s", &input_buffer);
             if (ret == EOF) {
                 break;
             }
             pthread_mutex_lock(&mutex);
-            input.value = (double)strtof(input_buffer, &pEnd);
-            input.available = true;
+            input_s.value = (double)strtof(input_buffer, &pEnd);
+            input_s.available = true;
             pthread_mutex_unlock(&mutex);
         }
     }
@@ -43,22 +43,31 @@ int main(int argc, const char* argv[]) {
     ncConfig.hidden_layers = ini_getl("Neural Network", "hidden_layers", -1, inifile);
     ncConfig.layers = ncConfig.hidden_layers + 2;
     ncConfig.learning_rate = round(ini_getf("Neural Network", "learning_rate", -1, inifile) * 100) / 100;
-    ncConfig.max_epochs = ini_getl("Neural Network", "max_epochs", -1, inifile);
     ncConfig.infinite = ini_getl("Neural Network", "infinite", -1, inifile);
+    if (ncConfig.infinite == 1)
+        ncConfig.max_epochs = -1;
+    else
+        ncConfig.max_epochs = ini_getl("Neural Network", "max_epochs", -1, inifile);
+    ncConfig.one_shot = ini_getl("Neural Network", "one_shot", -1, inifile);
+    if (ncConfig.one_shot == 1)
+        ncConfig.max_epochs = 1;
     ncConfig.neurons = ini_getl("Neural Network", "neurons", -1, inifile);
     ncConfig.output_layer_neurons = ini_getl("Neural Network", "output_layer_neurons", -1, inifile);
     ncConfig.setpoint = round(ini_getf("Neural Network", "setpoint", -1, inifile) * 100) / 100;
+    ncConfig.initialized = 0;
 
     double* error_array = calloc(ncConfig.max_epochs, sizeof(double));
     double x[ncConfig.max_epochs];
     double y[ncConfig.max_epochs];
     // input_st input[ncConfig.inputs];
-    input.available = false;
+    input_s.available = false;
 
     pthread_mutex_init(&mutex, NULL);
     pthread_create(&feedInputThread, NULL, feedInput, (void*)(&job));
 
-    learn_loop(&ncConfig, error_array, &input, time(NULL));
+    for (int i = 0; i < 500; i++) {
+        learn_loop(&ncConfig, error_array, &input_s, time(NULL));
+    }
 
     // for (int i = 0; i < ncConfig.max_epochs; i++) {
     //     x[i] = (float)i;
