e19b1103be432f7d5ad101d253800480200dcfbb
Jakob <jakyy701@gmail.com>
2026-03-24 00:51:00 +0100

    added possibility to debug CPython

--- scripts/testNC.py
+++ scripts/testNC.py
@@ -1,7 +1,7 @@
 import sys
 import neuralControllerInterface
 
-cfg = neuralControllerInterface.NeuralController(hidden_layers=2, layers=4, neurons=128,
+cfg = neuralControllerInterface.NeuralController(hidden_layers=2, layers=4, neurons=8,
     output_layer_neurons=1, inputs=10,
     max_epochs=5000, learning_rate=0.001, setpoint=1.0)
 print(cfg)
